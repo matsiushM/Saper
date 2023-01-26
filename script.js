465cc1385904925e8a107df75f9bf40c31a2f5e3
Максим <matiush2018@yandex.ru>
2023-01-27 01:26:10 +0300

    new game table and new system add cells

--- script.js
+++ script.js
@@ -1,25 +1,31 @@
-startGame(8,8,15);
+startGame(8, 8, 15);
+
 
 function startGame(height, width, bombs) {
-    const gameTable = document.querySelector('.gameTable');
-    const cellsCount = height * width;
-    gameTable.innerHTML = '<button></button>'.repeat(cellsCount);
-    const cells = [...gameTable.children];
-    const bomb = [...Array(cellsCount).keys()].sort(() => Math.random() - 0.5).slice(0, bombs);
 
-    gameTable.addEventListener('click',(event)=> {
-       if(event.target.tagName !== 'BUTTON'){
-           return;
-       }
-       const index = cells.indexOf(event.target);
-       const column = index % width;
-       const row = Math.floor(index/width);
-       event.target.innerHTML = isBomb(row,column) ? 'X' : ' ';
-       event.target.disabled = true;
+    const table = document.querySelector('.gameTable');
+    const cells = height * width;
+
+    const gameTable = Array.from({length: cells}, (e,index) => {
+        index++;
+        document.createElement('button');
+        return {
+            x: index % 10,
+            y: Math.floor(index / 10),
+            bomb: false,
+            element: document.createElement('button')
+        };
     });
-    function isBomb(row, colum){
-        const index = row * width + colum;
-        return bomb.includes(index);
-    }
+
+    gameTable.forEach((e) => {
+       table.insertAdjacentElement("afterbegin", e.element);
+    });
+
+    gameTable.forEach((e) => {
+
+    });
+
 }
+
+
 
